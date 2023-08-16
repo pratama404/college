a14f618ab547557e6b84a04502b12b407e5cd6b9
Ageng Putra Pratama <65237429+pratama404@users.noreply.github.com>
2023-08-16 12:59:52 +0700

    Update House.java

--- basic programming/House.java
+++ basic programming/House.java
@@ -25,3 +25,22 @@ public class House{
         }
     }
 }
+
+
+/*
+import java.util.*;
+public class House {
+	public static void main(String[] args) {
+		Scanner bi = new Scanner(System.in);		System.out.print("Harga Rumah (Juta) :");
+		int ru = bi.nextInt();
+		System.out.print("Tabungan Awal (Juta) :");
+		int ta = bi.nextInt();
+		System.out.print("Tabungan perbulan (Juta) :");
+		int tt = bi.nextInt();
+		while (ta  <=ru+ tt-1 ){
+			System.out.print(ta + " ");
+			ta+=tt;
+		}
+	}
+}
+*/
